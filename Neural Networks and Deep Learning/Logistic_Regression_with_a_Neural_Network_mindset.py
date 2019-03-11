dfa4e9472a05e80427c0921653307eae3d233045
Emanuel Pintilii <emanuel.pintilii@arcadis.com>
2019-03-11 18:22:30 +0200

    Update

--- Neural Networks and Deep Learning/Logistic_Regression_with_a_Neural_Network_mindset.py
+++ Neural Networks and Deep Learning/Logistic_Regression_with_a_Neural_Network_mindset.py
@@ -4,11 +4,8 @@ import h5py
 import scipy
 from PIL import Image
 from scipy import ndimage
-from lr_utils import load_dataset
-
-train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
-
 import h5py
+
 
 def load_dataset():
     train_dataset = h5py.File('datasets/train_catvnoncat.h5', "r")
@@ -23,14 +20,14 @@ def load_dataset():
     return train_set_x, train_set_y, test_set_x, test_set_y, classes
 
 
-train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes = load_dataset()
+train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
 
 index = 25
 plt.imshow(train_set_x_orig[index])
 #plt.show()
 
 
-print("y = ", train_set_y_orig[:,index], "it's a", classes[np.squeeze(train_set_y_orig[:, index])].decode("utf-8"))
+print("y = ", train_set_y[:,index], "it's a", classes[np.squeeze(train_set_y[:, index])].decode("utf-8"))
 
 m_train = train_set_x_orig.shape[0]
 m_test = test_set_x_orig.shape[0]
@@ -56,7 +53,7 @@ print ("test_set_y shape: " + str(test_set_y.shape))
 print ("sanity check after reshaping: " + str(train_set_x_flatten[0:5,0]))
 
 train_set_x = train_set_x_flatten / 255.
-test_set_x =test_set_x_flatten / 255.
+test_set_x = test_set_x_flatten / 255.
 
 def sigmoid(z):
     logistic = 1/(1+np.exp(-z))
@@ -71,7 +68,7 @@ def initialize_with_zeros(dim):
 
 def propagate(w, b, X, Y):
     m = X.shape[1]
-    A = sigmoid(np.dot(w.T) + b)
+    A = sigmoid(np.dot(w.T, X) + b)
     cost = (-1/m) * np.sum(Y*np.log(A) + (1-Y) * (np.log(1-A)))
     dw = (1/m) * np.dot(X, (A-Y).T)
     db = (1/m) * np.sum(A-Y)
@@ -82,6 +79,13 @@ def propagate(w, b, X, Y):
     grads = {"dw": dw,
              "db": db}
     return grads, cost
+
+w, b, X, Y = np.array([[1], [2]]), 2, np.array([[1,2], [3,4]]), np.array([[1, 0]])
+grads, cost = propagate(w, b, X, Y)
+print ("dw = " + str(grads["dw"]))
+print ("db = " + str(grads["db"]))
+print ("cost = " + str(cost))
+
 
 def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
     costs = []
@@ -106,7 +110,7 @@ params, grads, costs = optimize(w, b, X, Y, num_iterations=100, learning_rate=0.
 
 def predict(w, b, X):
     m = X.shape[1]
-    Y_prediction = np.zeros((1, m))
+    Y_prediction = np.zeros((1, m), dtype=np.int)
     w = w.reshape(X.shape[0], 1)
 
     A = sigmoid(np.dot(w.T, X)+b)
@@ -118,4 +122,51 @@ def predict(w, b, X):
 
 def model(X_train, Y_train, X_test, Y_test, num_iterations=2000, learning_rate = 0.5, print_cost = False):
     w, b = initialize_with_zeros(X_train.shape[0])
-    
+    parameters, grads, costs = optimize(w, b, X_train, Y_train, num_iterations, learning_rate, print_cost)
+    w = parameters["w"]
+    b = parameters["b"]
+    Y_prediction_train = predict(w, b, X_train)
+    Y_prediction_test = predict(w, b, X_test)
+    print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train-Y_train))*100))
+    print("test accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_test-Y_test))*100))
+    d = {"costs": costs,
+         "Y_prediction_train": Y_prediction_train,
+         "Y_prediction_test": Y_prediction_test,
+         "w": w,
+         "b": b,
+         "learning_rate": learning_rate,
+         "num_iterations": num_iterations}
+    return d
+
+d = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations=2000, learning_rate=0.005, print_cost=True)
+
+index = 5
+plt.imshow(test_set_x[:, index].reshape((num_px, num_px, 3)))
+print("y = " + str(test_set_y[0, index]) + ', you predicted that it is a \"' + str(classes[d["Y_prediction_test"][0, index]].decode("utf-8")) +  '\" picture.')
+
+# Plot learning curve (with costs)
+costs = np.squeeze(d['costs'])
+plt.plot(costs)
+plt.ylabel('cost')
+plt.xlabel('iterations (per hundreds)')
+plt.title("Learning rate =" + str(d["learning_rate"]))
+plt.show()
+
+learning_rates = [0.01, 0.001, 0.0001]
+models = {}
+for i in learning_rates:
+    print("learning rate is: " + str(i))
+    models[str(i)] = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations=1500
+                           , learning_rates = i, print_cost=False)
+    print("\n" + "------------------------------------------------------"  + "\n")
+
+for i in learning_rates:
+    plt.plot(np.squeeze(models[str(i)][costs]), label = str(models[str(i)]["learning_rate"]))
+
+plt.ylabel("cost")
+plt.xlabel("iterations")
+legend = plt.legend(loc = "upper center", shadow = True)
+frame = legend.get_frame()
+frame.set_facecolor("0.90")
+plt.show()
+
---
+++ Neural Networks and Deep Learning/Planar_data_classification_with_one_hidden_layer.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from testCases import *
+import sklearn
+import sklearn.datasets
+import sklearn.linear_model
+from planar_utils import plot_decision_boundary, sigmoid, load_planar_dataset, load_extra_datasets
+
+np.random.seed(1)
+
+X, Y = load_planar_dataset()
+
+
+plt.scatter(X[0, :], X[1, :], c=np.squeeze(Y), s=40, cmap=plt.cm.Spectral)
+#plt.show()
