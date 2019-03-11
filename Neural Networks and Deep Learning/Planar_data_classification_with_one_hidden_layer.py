d053b1fd8a8b1602866c498f6e642799969dcbcc
Emanuel <pintiliiemanuel@gmail.com>
2019-03-12 00:41:39 +0200

    Update

--- Neural Networks and Deep Learning/Planar_data_classification_with_one_hidden_layer.py
+++ Neural Networks and Deep Learning/Planar_data_classification_with_one_hidden_layer.py
@@ -10,6 +10,16 @@ np.random.seed(1)
 
 X, Y = load_planar_dataset()
 
-
 plt.scatter(X[0, :], X[1, :], c=np.squeeze(Y), s=40, cmap=plt.cm.Spectral)
 #plt.show()
+
+shape_X = X.shape
+shape_Y = Y.shape
+
+m = Y.shape[1]
+
+clf = sklearn.linear_model.LogisticRegressionCV()
+clf.fit(X.T, Y.T)
+
+
+
