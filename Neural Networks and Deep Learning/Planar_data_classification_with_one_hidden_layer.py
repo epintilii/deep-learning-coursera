f079fad051f7220fda1c08876844f511eb8ccaa3
Emanuel Pintilii <emanuel.pintilii@arcadis.com>
2019-03-12 17:17:34 +0200

    Update

--- Neural Networks and Deep Learning/Planar_data_classification_with_one_hidden_layer.py
+++ Neural Networks and Deep Learning/Planar_data_classification_with_one_hidden_layer.py
@@ -4,12 +4,119 @@ from testCases import *
 import sklearn
 import sklearn.datasets
 import sklearn.linear_model
-from planar_utils import plot_decision_boundary, sigmoid, load_planar_dataset, load_extra_datasets
+from planar_utils import sigmoid, load_extra_datasets
 
 np.random.seed(1)
 
-X, Y = load_planar_dataset()
+def load_planar_dataset():
+    np.random.seed(1)
+    m = 400  # number of examples
+    N = int(m / 2)  # number of points per class
+    D = 2  # dimensionality
+    X = np.zeros((m, D))  # data matrix where each row is a single example
+    Y = np.zeros((m, 1), dtype='uint8')  # labels vector (0 for red, 1 for blue)
+    a = 4  # maximum ray of the flower
 
+    for j in range(2):
+        ix = range(N * j, N * (j + 1))
+        t = np.linspace(j * 3.12, (j + 1) * 3.12, N) + np.random.randn(N) * 0.2  # theta
+        r = a * np.sin(4 * t) + np.random.randn(N) * 0.2  # radius
+        X[ix] = np.c_[r * np.sin(t), r * np.cos(t)]
+        Y[ix] = j
+
+    X = X.T
+    Y = Y.T
+
+    return X, Y
+
+def plot_decision_boundary(model, X, y):
+    # Set min and max values and give it some padding
+    x_min, x_max = X[0, :].min() - 1, X[0, :].max() + 1
+    y_min, y_max = X[1, :].min() - 1, X[1, :].max() + 1
+    h = 0.01
+    # Generate a grid of points with distance h between them
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    # Predict the function value for the whole grid
+    Z = model(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    # Plot the contour and training examples
+    plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
+    plt.ylabel('x2')
+    plt.xlabel('x1')
+    plt.scatter(X[0, :], X[1, :], c=y.ravel(), cmap=plt.cm.Spectral)
+    #plt.show()
+
+
+X, Y = load_planar_dataset()
 
 plt.scatter(X[0, :], X[1, :], c=np.squeeze(Y), s=40, cmap=plt.cm.Spectral)
 #plt.show()
+
+clf = sklearn.linear_model.LogisticRegressionCV()
+clf.fit(X.T, Y.T)
+
+plot_decision_boundary(lambda x: clf.predict(x), X, Y)
+plt.title("Logistic Regression")
+
+LR_predictions = clf.predict(X.T)
+print("LR_predictions shape is", LR_predictions.shape)
+print("Y shape is ", Y.shape)
+print("Accuracy of the logistic regression: %d" % float((np.dot(Y, LR_predictions) + np.dot(1-Y, 1-LR_predictions))/float(Y.size)*100)+
+      "%" + "(percentage of correctly labelled datapoints)")
+
+
+
+def layer_sizes(X, Y):
+    n_x = X.shape[0]
+    n_h = 4
+    n_y = Y.shape[0]
+    return (n_x, n_h, n_y)
+
+def initialize_parameters(n_x, n_h, n_y):
+    np.random.seed(2)
+    W1 = np.random.randn(n_h, n_x)*0.01
+    b1 = np.zeros((n_h, 1))
+    W2 = np.random.randn(n_y, n_h)*0.01
+    b2 = np.zeros((n_y, 1))
+
+    assert (W1.shape == (n_h, n_x))
+    assert (b1.shape == (n_h, 1))
+    assert (W2.shape == (n_y, n_h))
+    assert (b2.shape == (n_y, 1))
+
+    parameters = {"W1":W1,
+                  "b1":b1,
+                  "W2":W2,
+                  "b2":b2}
+    return parameters
+
+def forward_propagation(X, parameters):
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+
+    Z1 = np.dot(W1, X) + b1
+    A1 = np.tanh(Z1)
+    Z2 = np.dot(W2, A1) + b2
+    A2 = sigmoid(Z2)
+
+    assert (A2.shape == (1, X.shape[1]))
+
+    cache = {"Z1": Z1,
+             "A1": A1,
+             "Z2":Z2,
+             "A2":A2}
+
+    return A2, cache
+
+def compute_cost(A2, Y, parameters):
+    m = Y.shape[1]
+    W1 = parameters["W1"]
+    W2 = parameters["W2"]
+    logprobs = np.multiply(np.log(A2), Y) + np.multiply((1-Y), np.log(1-A2))
+    cost = - np.sum(logprobs)/m
+    cost = np.squeeze(cost)
+    assert (isinstance(cost, float))
+    return cost
+
