c796edeea87057915ec99fabd1507ba4ba6c648d
Emanuel <pintiliiemanuel@gmail.com>
2019-04-07 22:48:40 +0300

    Update

---
+++ Improving Deep Neural Networks Hyperparameter tuning, Regularization and Optimization/Optimization_methods.py
@@ -0,0 +1,79 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.io
+import math
+import sklearn
+import sklearn.datasets
+from testCases1 import *
+from opt_utils import load_params_and_grads, initialize_parameters, forward_propagation, backward_propagation
+from opt_utils import compute_cost, predict, predict_dec, plot_decision_boundary, load_dataset
+
+def update_parameters_with_gd(parameters, grads, learning_rate):
+    L = len(parameters) // 2
+    for l in range(L):
+        parameters["W"+str(l+1)] = parameters["W"+str(l+1)] - learning_rate *grads["dW"+str(l+1)]
+        parameters["b"+str(l+1)] = parameters["b"+str(l+1)] - learning_rate *grads["db"+str(l+1)]
+    return parameters
+
+def random_mini_batches(X, Y, mini_batch_size = 64, seed=1):
+    np.random.seed(seed)
+    m = X.shape[1]
+    mini_batches = []
+    permutation = list(np.random.permutation(m))
+    shuffled_X = X[:, permutation]
+    shuffled_Y = Y[:, permutation].reshape((1,m))
+
+    num_complete_minibatches = math.floor(m/mini_batch_size)
+
+    for k in range(num_complete_minibatches):
+        mini_batch_X = shuffled_X[:, k*mini_batch_size:(k+1)*mini_batch_size]
+        mini_batch_Y = shuffled_Y[:, k*mini_batch_size:(k+1)*mini_batch_size]
+        mini_batch = (mini_batch_X, mini_batch_Y)
+        mini_batches.append(mini_batch)
+
+    if m % mini_batch_size != 0 :
+        end = m - mini_batch_size*math.floor(m/mini_batch_size)
+        mini_batch_X = shuffled_X[:, num_complete_minibatches*mini_batch_size:]
+        mini_batch_Y = shuffled_Y[:, num_complete_minibatches*mini_batch_size:]
+        mini_batch = (mini_batch_X, mini_batch_Y)
+        mini_batches.append(mini_batch)
+
+    return mini_batches
+
+def initialize_velocity(parameters):
+    L = len(parameters) // 2
+    v = {}
+    for l in range(L):
+        v["dW"+str(l+1)] = np.zeros_like(parameters["W"]+ str(l+1))
+        v["db"+str(l+1)] = np.zeros_like(parameters["b"]+ str(l+1))
+    return v
+
+def update_parameters_with_momentum(parameters, grads, v, beta, learning_rate):
+    L = len(parameters)
+    for l in range(L):
+        v["dW" + str(l+1)] = beta * v["dW" + str(l+1)] + (1-beta) * grads["dW" + str(l+1)]
+        v["db" + str(l+1)] = beta * v["db" + str(l+1)] + (1-beta) * grads["db" + str(l+1)]
+        parameters["dW" + str(l+1)] = parameters["dW" + str(l+1)] - learning_rate*v["dW" + str(l+1)]
+        parameters["db" + str(l+1)] = parameters["db" + str(l+1)] - learning_rate*v["db" + str(l+1)]
+    return parameters, v
+
+
+def initialize_adam(parameters):
+    L = len(parameters) // 2
+    v = {}
+    s = {}
+    for l in range(L):
+        v["dW" + str(l+1)] = np.zeros_like(parameters["W" + str(l+1)])
+        v["db" + str(l+1)] = np.zeros_like(parameters["b" + str(l+1)])
+        s["dW" + str(l+1)] = np.zeros_like(parameters["W" + str(l+1)])
+        s["db" + str(l+1)] = np.zeros_like(parameters["b" + str(l+1)])
+    return v, s
+
+
+
+
+
+
+
+
+
