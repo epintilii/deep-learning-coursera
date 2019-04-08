8c6dbb82233cb198e957e4fcc2ab1bfca1aacd47
Emanuel <pintiliiemanuel@gmail.com>
2019-04-08 23:34:26 +0300

    Update

--- Improving Deep Neural Networks Hyperparameter tuning, Regularization and Optimization/Gradient_checking.py
+++ Improving Deep Neural Networks Hyperparameter tuning, Regularization and Optimization/Gradient_checking.py
@@ -31,5 +31,59 @@ def gradient_check(x, theta, epsilon = 1e-7):
 
     return difference
 
+def forward_propagation(X, Y, parameters):
+    m = X.shape[1]
+    W1 = parameters["W1"]
+    W2 = parameters["W2"]
+    W3 = parameters["W3"]
+    b1 = parameters["b1"]
+    b2 = parameters["b2"]
+    b3 = parameters["b3"]
+
+    Z1 = np.dot(W1, X) + b1
+    A1 = relu(Z1)
+    Z2 = np.dot(W2, A1) + b2
+    A2 = relu(Z2)
+    Z3 = np.dot(W3, A2) + b3
+    A3 = sigmoid(Z3)
+
+    logprobs = np.multiply(-np.log(A3), Y) + np.multiply(-np.log(1-A3), (1-Y))
+    cost = 1 / m *np.sum(logprobs)
+
+    cache = (Z1, A1, W1, b1, Z2, A2, W2, b2, Z3, A3, W3, b3)
+
+    return  cost, cache
+
+def backward_propagation(X, Y, cache):
+    m = X.shape[1]
+    (Z1, A1, W1, b1, Z2, A2, W2, b2, Z3, A3, W3, b3) = cache
+
+    dZ3 = A3 - Y
+    dW3 = 1. / m * np.dot(dZ3, A2.T)
+    db3 = 1. / m * np.sum(dZ3, axis=1, keepdims=True)
+
+    dA2 = np.dot(W3.T, dZ3)
+    dZ2 = np.multiply(dA2, np.int64(A2>0))
+    dW2 = 1./m * np.dot(dZ2, A1.T)
+    db2 = 1./m * np.sum(dZ2, axis=1, keepdims=True)
+
+    dA1 = np.dot(W2.T, dZ2)
+    dZ1 = np.multiply(dA1, np.int64(A1>0))
+    dW1 = 1./m * np.dot(dZ1, X.T)
+    db1 = 1./m * np.dot(dZ1, X.T)
+
+    gradients = {"dZ3": dZ3, "dW3": dW3, "db3": db3,
+                 "dA2": dA2, "dZ2": dZ2, "dW2": dW2, "db2": db2,
+                 "dA1": dA1, "dZ1": dZ1, "dW1": dW1, "db1": db1}
+
+    return gradients
+
+
+
+
+
+
+
+
 
 
