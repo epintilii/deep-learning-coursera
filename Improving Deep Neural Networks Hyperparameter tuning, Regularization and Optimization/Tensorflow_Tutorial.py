6f2ff0d58f5c76c5f8e077b541e076adb9510849
Emanuel <pintiliiemanuel@gmail.com>
2019-04-23 00:45:52 +0300

    Update

--- Improving Deep Neural Networks Hyperparameter tuning, Regularization and Optimization/Tensorflow_Tutorial.py
+++ Improving Deep Neural Networks Hyperparameter tuning, Regularization and Optimization/Tensorflow_Tutorial.py
@@ -96,5 +96,22 @@ def create_placeholders(n_x, n_y):
     return X, Y
 
 
+def initialize_parameters():
+    tf.set_random_seed(1)
 
+    W1 = tf.get_variable("W1", [25, 12288], initializer=tf.contrib.layers.xavier_initializer(seed=1))
+    b1 = tf.get_variable("b1", [25, 1], initializer=tf.zeros_initializer())
 
+    W2 = tf.get_variable("W2", [12, 25], initializer=tf.contrib.layers.xavier_initializer(seed=1))
+    b2 = tf.get_variable("b2", [12, 1], initializer=tf.zeros_initializer())
+
+    W3 = tf.get_variable("W3", [6, 12], initializer=tf.contrib.layers.xavier_initializer(seed=1))
+    b3 = tf.get_variable("b3", [6, 1], initializer=tf.zeros_initializer())
+
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2,
+                  "W3": W3,
+                  "b3": b3}
+    return parameters
